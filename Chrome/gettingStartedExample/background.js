f8fead590980199fa3a3bb4edd9c1708f8ffbcb7
詹伟超 <wczhan@ctrip.com>
2020-11-09 18:04:54 +0800

    code

--- Chrome/gettingStartedExample/background.js
+++ Chrome/gettingStartedExample/background.js
@@ -1,22 +1,23 @@
+const rule1 = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {
+        hostEquals: 'www.google.com',
+        schemes: ['https']
+      }
+    })
+  ],
+  actions: [
+    new chrome.declarativeContent.ShowPageAction()
+  ]
+};
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({color: '#3aa757'}, () => {
     console.log('The color is green');
   });
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {
-              hostEquals: '*'
-            }
-          })
-        ],
-        actions: [
-          new chrome.declarativeContent.ShowPageAction()
-        ]
-      }
-    ]);
+    chrome.declarativeContent.onPageChanged.addRules([rule1]);
   });
 });
